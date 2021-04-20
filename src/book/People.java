a94e3d19991d10e0fd86c229d53d60433ea0d7f2
Cathy1105 <974900150@qq.com>
2021-04-20 21:30:28 +0800

    aaa

---
+++ src/book/People.java
@@ -0,0 +1,17 @@
+package book;
+
+public class People {
+    protected double weight,height;
+    {
+        System.out.println("yayayaya");
+    }
+    {
+        height=173;
+        System.out.println("average height:"+height);
+    }
+    {
+        weight=70;
+        System.out.println("average weight:"+weight);
+    }
+}
+
